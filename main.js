41d9f9265eab4a220a0068b3668a1431b1df6afa
v-kondratiev <viktor.kondratiev@gmail.com>
2023-03-27 19:34:14 +0100

    add checkbox style add registration styles

---
+++ main.js
@@ -0,0 +1,13 @@
+let container = document.querySelector(".container");
+let pwShowHide = document.querySelector(".pwShowHide");
+let pwField = document.querySelector(".password");
+
+///////Show/Hide password and change icon/////
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        pwFields.
+
+    })
+
+})
