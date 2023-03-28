47c9e64bd0f07bf19e226b620c2cabfcf0b2d6c6
v-kondratiev <viktor.kondratiev@gmail.com>
2023-03-28 09:30:27 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-let container = document.querySelector(".container");
-let pwShowHide = document.querySelector(".pwShowHide");
-let pwField = document.querySelector(".password");
+
+let pwShowHide = document.querySelectorAll(".pwShowHide");
+let pwFields = document.querySelectorAll(".password");
 
 ///////Show/Hide password and change icon/////
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
-        pwFields.
+        pwFields.forEach(pwField => {
+            if (pwField.type === "password") {
+                pwField.type = "text";
+
+                pwShowHide.forEach(eyeIcon => {
+                    pwShowHide.classList.replace("uil-eye-slash", "uil uil-eye");
+                })
+
+            } else {
+                pwField.type = "password";
+            }
+
+        })
 
     })
 
