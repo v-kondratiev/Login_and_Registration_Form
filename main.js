4a0bfdff845065901bd11bc07063e5ae2c645b08
v-kondratiev <viktor.kondratiev@gmail.com>
2023-04-03 15:26:15 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-
-let pwShowHide = document.querySelectorAll(".showHidePw");
-let pwFields = document.querySelectorAll(".password");
+const container = document.querySelector('.container');
+const pwShowHide = document.querySelectorAll(".showHidePw");
+const pwFields = document.querySelectorAll(".password");
+const signUp = document.querySelector(".signup-link");
+const login = document.querySelector(".login-link");
 
 ///////Show/Hide password and change icon/////
 
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
+pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
         pwFields.forEach(pwField => {
             if (pwField.type === "password") {
                 pwField.type = "text";
 
-                pwShowHide.forEach(eyeIcon => {
-                    pwShowHide.classList.replace("uil-eye-slash", "uil uil-eye");
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace("uil-eye-slash", "uil-eye");
                 })
 
             } else {
                 pwField.type = "password";
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace("uil-eye", "uil-eye-slash");
+                })
             }
-
         })
-
     })
-
 })
+
+///////Show sign up form/////
+
+
+signUp.addEventListener("click", ( )=>{
+    container.classList.add("active");
+});
+login.addEventListener("click", ( )=>{
+    container.classList.remove("active");
+});
+
